54a8af4232fc9fb6d4d015077064b38c3f6bc33a
Sara Cristina Ferreira <42873969+ferreirasara@users.noreply.github.com>
2019-05-20 18:37:01 -0300

    A sub-rotina 'cifra' lê corretamente a chave e o arquivo.

--- cifra.s
+++ cifra.s
@@ -12,40 +12,54 @@ cifra:
 
 	;mov 	esi, dword[ebp + 16]	; char* chave
 	;mov 	edx, dword[ebp + 12]	; int fd_saida
-	mov 	esi, 0 					; percorre o arquivo
+	mov 	esi, 0 					; percorre o arquivo (nunca vai ser zerado)
 
 zera_edi:
 	mov 	edi, 0					; percorre a chave
+	jmp 	le_arquivo
 
 le_chave:
-	mov 	ebx, dword[ebp + 16] 	; endereco incial da chave
-	cmp 	dword[ebx], 0
+	mov 	edx, dword[ebp + 16]	; endereco inicial da chave
+	mov 	ebx, dword[edx + edi]	; caractere da vez
+	mov 	dword[chave], ebx		; move o caractere pra variavel chave
+	cmp 	dword[chave], 0 		; terminador nulo?
+	je 		zera_edi 				; se a chave acabou, zera o edi e volta para o comeco da mesma
+	inc 	edi						; incrementa para pegar o proximo caractere
+	jmp 	faz_cifra 				; usa o cactere atual na cifragem
 
 le_arquivo:
+	push 	edx 					; salva o endereco inicial da chave
+
 	mov 	eax, 3 					; sys_read
 	mov 	ebx, dword[ebp + 8]		; int fd_entrada
 	mov 	ecx, c					; char c;
-	mov 	edx, 1					; tamanho
-	int 	80h						; kernel
-
-	cmp 	dword[c], '0'			; terminador nulo?
-	je 		termina
-	jne 	imprime
+	mov 	edx, 1					; tamanho (um caractere por vez)
+	int 	80h						; chama o kernel
 
 
-imprime:
+	cmp 	dword[c], '0'			; nao sei como compara com EOF, ai precisa ter um '0' no final do arquivo
+	je 		termina 				; se encontra o zero, sai da sub-rotina
+
+	pop 	edx 					; volta para edx o enredero inicial da chave
+	jmp 	le_chave
+
+faz_cifra:
+	;add 	dword[c], 1 			; funciona isso de somar um número com o cod ascii :)
+
 	push 	dword[c]
+	push 	str_c
+	call 	printf
+	add 	esp, 8
+	push 	dword[chave]
 	push 	str_c
 	call 	printf
 	add 	esp, 8
 	jmp 	le_arquivo
 
-termina:
-	push 	dword[ebp + 16]
-	push 	str_s
-	call 	printf
-	add 	esp, 8
+escreve_arquivo:
 
+
+termina:
 	pop     edi
 	pop     esi
     pop     ebx
@@ -55,7 +69,7 @@ termina:
 
 section .bss
 	c: 		resd 1
+	chave 	resd 1
 
 section .data
-	str_s 	db ' %s'
-	str_c	db '%c'
+	str_c	db '[%c]'
