e80578f7d817403714bca08c69ea91889e2da95a
Sara Ferreira <ferreirasara1501@hotmail.com>
2019-05-24 12:58:29 -0300

    Macros alteradas para trabalhar com letras maiúsculas e minúscualas. Criação de script para mostrar os arquivos em tela.

---
+++ mostra_arquivos.sh
@@ -0,0 +1,10 @@
+echo "Entrada para cifra: "
+cat entrada_cifra.txt
+echo "Entrada para decifra: "
+cat entrada_decifra.txt
+echo "Saida da cifra: "
+cat saida_cifra.txt
+echo ""
+echo "Saida da decifra: "
+cat saida_decifra.txt
+echo ""
